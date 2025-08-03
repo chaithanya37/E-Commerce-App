1dbbad4b407bc3605e558e3866230ba5dee98220
chaithanya37 <chaithanyakanipakam37@gmail.com>
2025-08-03 10:51:35 +0530

    URL Updated

--- ecommerce/vite.config.js
+++ ecommerce/vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
-import dotenv from './dotenv'
+import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
-  plugins: [react(
+  plugins: [
+    react(),
     tailwindcss(),
-  )],
+  ],
   server: {
     proxy: {
-      '/api': process.env.BACKEND_URL, // Your backend port
-    }
-  }
+      '/api': {
+        target: process.env.BACKEND_URL, 
+        changeOrigin: true,
+      },
+    },
+  },
 });
